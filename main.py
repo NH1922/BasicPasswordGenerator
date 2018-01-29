f5eb8a48faaea95f8661cf5d25ebcecd00fa91c0
NH1922 <31303415+NH1922@users.noreply.github.com>
2018-01-30 02:14:20 +0530

    Retrive Function Added
    - Added 3 screens to perform add and retrieve functions
    - Switched to kivy.core.clipboard to perform clipboard functions instead of pyperclip

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#imports 
-
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
@@ -7,58 +5,81 @@ from kivy.properties import StringProperty
 from kivy.properties import NumericProperty
 from random import choice
 from random import shuffle
-from pyperclip import copy
+#from pyperclip import copy
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager,Screen,FadeTransition
+from kivy.core.clipboard import Clipboard
 
 #Dictionary to store the usernames and passwords
 IDS ={}
 
+
 class MainScreen(Screen):
     pass
 class GenerateScreen(Screen):
-    pass
-class screenmanage(ScreenManager):
-    pass
-
-
-#Class to generate the passwords
-class Dis(GridLayout):
     global IDS
     length = NumericProperty()
     password = StringProperty('')
     keyword = StringProperty('')
+
     def generatepwd(self):
         self.password = ''
         keywd = self.ids.keywd.text
 
         alphas = 'ABCDEFGHIJKLMNOPQRSTUBWXYZabcdefghijklmnopqrstuvwxyz0123456789./_-'
-        if (self.ids.pwdlen.text==''):
-            popup = Popup(title="Incorrect Input",size=(200, 200),size_hint=(None, None),content=Label(text='Length cannot be empty ! '))
+        if (self.ids.pwdlen.text == ''):
+            popup = Popup(title="Incorrect Input", size=(200, 200), size_hint=(None, None),
+                          content=Label(text='Length cannot be empty ! '))
             popup.open()
         else:
             length = int(self.ids.pwdlen.text) - len(keywd)
 
-
-            while(length):
-               self.password=self.password + choice(alphas)
-               length = length - 1
+            while (length):
+                self.password = self.password + choice(alphas)
+                length = length - 1
             result = list(self.password)
             result.append(keywd)
             shuffle(result)
             result = ''.join(result)
             self.password = result
 
-#class to save the password and copy it to the clipboard using pyperclip
+            # class to save the password and copy it to the clipboard using pyperclip
+
     def savepwd(self):
         IDS[self.ids.usrname.text] = self.password
-        copy(IDS[self.ids.usrname.text])
-        popup = Popup(title='Saved !', content=Label(text='Copied to clipboard'),size_hint=(None,None),size=(400,200))
+        #copy(IDS[self.ids.usrname.text])
+        Clipboard.copy(self.password)
+        popup = Popup(title='Saved !', content=Label(text='Copied to clipboard'), size_hint=(None, None),
+                      size=(400, 200))
         popup.open()
 
     def clear(self):
-        self.length=0
-        #self.password = ''
+        self.length = 0
+        # self.password = ''
+
+class RetrieveScreen(Screen):
+    global IDS
+    def Fetch(self):
+            if(self.ids.ret_usrname.text in IDS):
+                pwd = IDS[self.ids.ret_usrname.text]
+                Clipboard.copy(pwd)
+                popup = Popup(title='Saved !', content=Label(text='Copied to clipboard'), size_hint=(None, None),size=(400, 200))
+                popup.open()
+            else:
+                popup = Popup(title='Error', content=Label(text='Username Not Found'), size_hint=(None, None),
+                              size=(400, 200))
+                popup.open()
+
+
+
+class screenmanage(ScreenManager):
+    pass
+
+
+
+
+
+
 
 class someapp(App):
     pass
